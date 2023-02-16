3a36e96d94edca086562e12184c76e81537c4bb2
Lucifer-0427 <harshpanchal2704@gmail.com>
2023-02-16 10:26:57 +0530

    merge

---
+++ Task-1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+
+{
+	     printf("----------------------------Menu----------------------\n");
+     printf("\t\t\tProduct\t  price\n ");
+     printf("\t\t\t1.Apple\t  90000rs\n");
+     printf("\t\t\t2.Samsung  70000rs\n");
+     printf("\t\t\t3.Vivo\t   45000rs\n");
+    
+}
